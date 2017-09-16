6c70485d286643fddcbfa60c5a38dc3b912817ad
Scott Kerkvliet <scott_kerkvliet@hotmail.com>
2017-09-15 22:30:25 -0400

    Add default values for pokemon array

---
+++ app/common/pokemon.js
@@ -0,0 +1,7 @@
+export default {
+    pokemon: [
+        {id: 63, name: "Abra", gen_1_stats: {hp: 25, attack: 20, defense: 15, speed: 90, special: 105}, gen_2_stats: {hp: 25, attack: 20, defense: 15, speed: 90, spc_attack: 105, spc_defense: 55}},
+        {id: 132, name: "Ditto", gen_1_stats: {hp: 48, attack: 48, defense: 48, speed: 48, special: 48}, gen_2_stats: {hp: 48, attack: 48, defense: 48, speed: 48, spc_attack: 48, spc_defense: 48}},
+        {id: 158, name: "Totodile", gen_1_stats: null, gen_2_stats: {hp: 50, attack: 65, defense: 64, spc_attack: 44, spc_defense: 48, speed: 43}}
+    ]
+}
