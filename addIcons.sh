c944ee1c8fd5920b90420b9558b4f1c0dec20bb0
Scott Kerkvliet <scott_kerkvliet@hotmail.com>
2017-09-29 02:26:53 -0400

    Added script for android icons

---
+++ addIcons.sh
@@ -0,0 +1,4 @@
+cp app/assets/App\ Icons/mdpi.png android/app/src/main/res/mipmap-mdpi/ic_launcher.png
+cp app/assets/App\ Icons/hdpi.png android/app/src/main/res/mipmap-hdpi/ic_launcher.png
+cp app/assets/App\ Icons/xhdpi.png android/app/src/main/res/mipmap-xhdpi/ic_launcher.png
+cp app/assets/App\ Icons/xxhdpi.png android/app/src/main/res/mipmap-xxhdpi/ic_launcher.png
